f943681ac353dc61a0d4ca54e99d156fa07a12b9
magoos140 <67796145+magoos140@users.noreply.github.com>
2024-11-03 09:39:35 -0500

    database connection

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('personal_expenses', 'root', '12345678', {
-  host: 'localhost',
+const sequelize = new Sequelize('railway', 'root', 'UrvWpHpUFvxttqJdrcsgBmIjNeftoTcf', {
+  host: 'autorack.proxy.rlwy.net',
+  port: 35608,
   dialect: 'mysql',
 });
 
